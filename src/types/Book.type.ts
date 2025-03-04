3ef859e17f4f2468904742703c2f6aa2882bbe03
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-03-04 22:15:53 +0200

    add Book type

---
+++ src/types/Book.type.ts
@@ -0,0 +1,10 @@
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  category: string;
+  isbn: string;
+  createdAt: string;
+  modifiedAt: string;
+  active: boolean;
+}
